23d90ce0ec51b0746366cb5e5af5378bea4c3a1d
Dannar Mawardi <dMawardi@gmail.com>
2025-04-17 12:53:27 +0800

    Added login button to nav bar and updated Job request form to behave differently for logged in user

--- resources/views/components/forms/job_request.blade.php
+++ resources/views/components/forms/job_request.blade.php
@@ -12,21 +12,26 @@
                             <!-- Contact Name -->
                             <div>
                                 <x-input-label for="contact_name" :value="__('Contact Name')" />
-                                <x-text-input id="contact_name" name="contact_name" type="text" class="mt-1 block w-full" :value="old('contact_name')" required autofocus autocomplete="name" />
+                                <x-text-input id="contact_name" name="contact_name" type="text" class="mt-1 block w-full" :value="old('contact_name', auth()->check() ? auth()->user()->name : '')" required autofocus autocomplete="name" />
                                 <x-input-error :messages="$errors->get('contact_name')" class="mt-2" />
                             </div>
 
                             <!-- Contact Email -->
                             <div>
                                 <x-input-label for="contact_email" :value="__('Contact Email')" />
-                                <x-text-input id="contact_email" name="contact_email" type="email" class="mt-1 block w-full" :value="old('contact_email')" required autocomplete="email" />
+                                @auth
+                                    <x-text-input id="contact_email" name="contact_email" type="email" class="mt-1 block w-full bg-gray-100" value="{{ auth()->user()->email }}" readonly disabled />
+                                    <p class="mt-1 text-xs text-gray-500">Email address is linked to your account and cannot be changed</p>
+                                @else
+                                    <x-text-input id="contact_email" name="contact_email" type="email" class="mt-1 block w-full" :value="old('contact_email')" required autocomplete="email" />
+                                @endauth
                                 <x-input-error :messages="$errors->get('contact_email')" class="mt-2" />
                             </div>
 
                             <!-- Contact Phone -->
                             <div>
                                 <x-input-label for="contact_phone" :value="__('Contact Phone')" />
-                                <x-text-input id="contact_phone" name="contact_phone" type="tel" class="mt-1 block w-full" :value="old('contact_phone')" required autocomplete="tel" />
+                                <x-text-input id="contact_phone" name="contact_phone" type="tel" class="mt-1 block w-full" :value="old('contact_phone', auth()->check() ? auth()->user()->phone : '')" required autocomplete="tel" />
                                 <x-input-error :messages="$errors->get('contact_phone')" class="mt-2" />
                             </div>
                         </div>
