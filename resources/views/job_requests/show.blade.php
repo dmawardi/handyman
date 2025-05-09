9a1285fe1637913b7d99b51ed355a83a02a969cb
Dannar Mawardi <dMawardi@gmail.com>
2025-05-09 15:20:31 +0800

    Back to all requests for normal users takes you back to dashboard instead of job request index

--- resources/views/job_requests/show.blade.php
+++ resources/views/job_requests/show.blade.php
@@ -5,7 +5,7 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Job Request Details') }}
             </h2>
-            <a href="{{ route('job-requests.index') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
+            <a href="{{ route('dashboard') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
                 {{ __('Back to All Requests') }}
             </a>
         </div>
