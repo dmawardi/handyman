97b45ababdd380eca9171ea24e61214a319fb808
Dannar Mawardi <dMawardi@gmail.com>
2025-05-09 14:07:22 +0800

    Made user job requests in order of most recent on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,8 +10,11 @@ class DashboardController extends Controller
     {
         // Obtain the authenticated user
         $user = auth()->user();
-        // Get all assigned job requests for the user
-        $jobRequests = $user->jobRequests()->with('attachments')->get();
+        // Get all assigned job requests for the user and sort by created_at in descending order
+        $jobRequests = $user->jobRequests()
+            ->orderBy('created_at', 'desc')
+            ->get();
+        // Return the dashboard view with the job requests
         return view('dashboard', compact('jobRequests'));
     }
 }
