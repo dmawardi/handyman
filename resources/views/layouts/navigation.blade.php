33f700c129c002dd96475e58f8a9b8b76da2215b
Dannar Mawardi <dMawardi@gmail.com>
2025-05-14 18:30:45 +0800

    Dashboard link takes you to different routes depending on type of user

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,8 +16,11 @@
             <div class="hidden space-x-4 sm:-my-px sm:ms-10 sm:flex">
                 {{-- AUTH --}}
                 @auth
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-white hover:text-secondary">
-                        {{ __('Dashboard') }}
+                {{-- Will take you to admin dashboard or basic depending on role --}}
+                    <x-nav-link 
+                    :href="auth()->user()->user_type == 'admin' ? route('admin.dashboard') : route('dashboard')" 
+                    :active="request()->routeIs('dashboard') || request()->routeIs('admin.dashboard')" class="text-white hover:text-secondary">
+                        {{  __('Dashboard') }}
                     </x-nav-link>
                 @endauth
 
