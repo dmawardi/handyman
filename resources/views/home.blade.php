1fa90420f00ca061c084a95c103ce897d842a9b9
Dannar Mawardi <dMawardi@gmail.com>
2025-05-25 12:45:25 +0800

    Updated meta description of home page. Removed 'handyman'->'rumahfix'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout title="RumahFix Bali | Maintenance Services in Bali" 
-description="Handyman Bali is your go-to destination for all handyman services in Bali. We offer a wide range of services including plumbing, electrical work, carpentry, and more. Our team of skilled professionals is dedicated to providing top-notch service to ensure your home or business is in perfect condition."
+description="RumahFix Bali is your go-to destination for all handyman services in Bali. We offer a wide range of services including plumbing, electrical work, carpentry, and more. Our team of skilled professionals is dedicated to providing top-notch service to ensure your home or business is in perfect condition."
 keywords="handyman, maintenance, maintenance requests, plumbing, electrician" canonical="url('/')">
     
     <x-whatsapp-button />
